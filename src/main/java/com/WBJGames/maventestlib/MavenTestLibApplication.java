50559dbd1af73815ca00682a857b51afccc87959
Bijian Wu <384318185@qq.com>
2023-04-26 23:01:07 +0100

    initialise project

---
+++ src/main/java/com/WBJGames/maventestlib/MavenTestLibApplication.java
@@ -0,0 +1,13 @@
+package com.WBJGames.maventestlib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenTestLibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenTestLibApplication.class, args);
+	}
+
+}
